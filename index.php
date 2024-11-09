13809eb51f848b574bc73e92f0e6445144dd4768
polimpo4 <polimpo4@hotmail.com>
2024-11-09 11:28:07 -0100

    New project version, structure and concept

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fituckness - Your way to life</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <script src="./assets/js/script.js" defer="defer"></script>
+    <link rel="shortcut icon" href="./assets/midias/favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <nav></nav>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
